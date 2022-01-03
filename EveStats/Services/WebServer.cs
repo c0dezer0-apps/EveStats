326b1caf34f0e5ed016a4894bc248b94226ece08
Brian Blankenship <c0dezer019@zohomail.com>
2022-01-03 04:36:07 -0600

    Interface for the HTTP Listener.

---
+++ EveStats/Services/WebServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EveStats.Services
+{
+    interface ISSOWebServer : IDisposable
+    {
+        void WaitForCode(string state, Action<Task<string>> callback);
+        void Start();
+        void Stop();
+        Task<string> WaitForCodeAsync(string state);
+    }
+}
