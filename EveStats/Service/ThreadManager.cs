de4973298d4d6ee138f32092f7c1457d9853085a
Brian Blankenship <c0dezer019@zohomail.com>
2022-01-04 20:36:19 -0600

    Creates and runs threads for tasks.

---
+++ EveStats/Service/ThreadManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace EveStats.Service
+{
+    public class ThreadManager
+    {
+        private static int sleepFor = 5000;
+        
+        
+
+
+    }
+}
