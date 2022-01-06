49361d4b2b5789fee9f1da0600c501c4a56a2388
Brian Blankenship <c0dezer019@zohomail.com>
2022-01-05 22:38:56 -0600

    Renamed and created a base HttpClient.

---
+++ EveStats/Service/Web/HttpClientService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using EveStats.Data.Resources;
+
+namespace EveStats.Services.Web
+{
+    /// <summary>
+    /// Implementing a base web client for making API calls.
+    /// </summary>
+
+    public class HttpClientService
+    {
+        static HttpClient client;
+
+        private static Uri SSOBase => new(APIConstants.SSOBase);
+        private static Uri ESI_BASE => new(APIConstants.ESIBase);
+        private static TimeSpan RTimeout => TimeSpan.FromSeconds(30);
+
+        /// <summary>
+        /// Creates a new instance of the HttpClient set to be a service for either SSO or ESI.
+        /// </summary>
+        /// <param name="receiver"></param>
+        public HttpClientService(string receiver)
+        {
+            client = new();
+            client.BaseAddress = receiver == "SSO" ? SSOBase : ESI_BASE;
+            // Making sure there are no unnecessary Headers
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.Timeout = RTimeout;
+        }
+
+        public void ShowResponse(HttpResponseMessage response)
+        {
+
+        }
+    }
+}
