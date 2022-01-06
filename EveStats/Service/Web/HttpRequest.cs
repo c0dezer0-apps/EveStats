34fec7b55db6e5cf16c6ac189ee657762d989804
Brian Blankenship <c0dezer019@zohomail.com>
2022-01-05 22:39:44 -0600

    Create HttpRequest.cs

---
+++ EveStats/Service/Web/HttpRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace EveStats.Service.Web
+{
+    /// <summary>
+    /// The parent class for making requests using the HttpClientService.
+    /// </summary>
+    public class HttpRequest
+    {
+    }
+}
