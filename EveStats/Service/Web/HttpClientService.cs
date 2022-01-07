77ff14432e7438707e86b836e08dc15a277eb3ae
Brian Blankenship <c0dezer019@zohomail.com>
2022-01-07 12:53:17 -0600

    Providing support commands.

--- EveStats/Service/Web/HttpClientService.cs
+++ EveStats/Service/Web/HttpClientService.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
 using EveStats.Data.Resources;
 
-namespace EveStats.Services.Web
+namespace EveStats.Service.Web
 {
     /// <summary>
     /// Implementing a base web client for making API calls.
@@ -18,9 +13,9 @@ namespace EveStats.Services.Web
     {
         static HttpClient client;
 
-        private static Uri SSOBase => new(APIConstants.SSOBase);
-        private static Uri ESI_BASE => new(APIConstants.ESIBase);
-        private static TimeSpan RTimeout => TimeSpan.FromSeconds(30);
+        protected static Uri SSOBase => new(APIConstants.SSOBase);
+        protected static Uri ESI_BASE => new(APIConstants.ESIBase);
+        protected static TimeSpan RTimeout => TimeSpan.FromSeconds(30);
 
         /// <summary>
         /// Creates a new instance of the HttpClient set to be a service for either SSO or ESI.
@@ -37,9 +32,29 @@ namespace EveStats.Services.Web
             client.Timeout = RTimeout;
         }
 
-        public void ShowResponse(HttpResponseMessage response)
+        /// <summary>
+        /// Cancel all requests.
+        /// </summary>
+        public void Cancel()
         {
+            client.CancelPendingRequests();
+        }
 
+        /// <summary>
+        /// Release and dispose of resources.
+        /// </summary>
+        public static void Dispose()
+        {
+            client.Dispose();
+        }
+
+        /// <summary>
+        /// Verifies endpoint.
+        /// </summary>
+        /// <returns>string</returns>
+        public static string Mode()
+        {
+            return client.BaseAddress == SSOBase ? "SSO" : "ESI";
         }
     }
 }
