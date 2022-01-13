6d477e4d4aa7af7a9ad912592e40bec178fe1286
Brian Blankenship <c0dezer019@zohomail.com>
2022-01-12 23:36:08 -0600

    Create ESIScopeListGenerator.cs

---
+++ EveStats/Service/Helpers/Web/ESIScopeListGenerator.cs
@@ -0,0 +1,15 @@
+﻿using EveStats.Data.Resources;
+
+namespace EveStats.Service.Helpers.Web
+{
+    public class ESIScopeListGenerator
+    {
+
+        public static string[] ListOfScopes()
+        {
+            string scopes = APIConstants.ESIScopes;
+
+            return scopes.Split(' ');
+        }
+    }
+}
